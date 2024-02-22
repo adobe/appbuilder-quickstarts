85561fcf3c164c45bd014ff27e0d811f47fd3b53
Jesse MacFadyen <purplecabbage@gmail.com>
2024-02-22 14:01:54 -0800

    added pwa sample app (#7)
    * added pwa sample app
    
    * removed workflows, added sample service worker
    
    * added license headers, updated readme

---
+++ progressive-web-app/README.md
@@ -0,0 +1,19 @@
+# App Builder PWA demo
+
+This is a basic PWA app. It includes typical pwa meta in index.html and icons.
+index.html has a script to register a service-worker.
+
+## Try it
+
+`aio app init --repo adobe/appbuilder-quickstarts/progressive-web-app`
+
+## Setup
+
+- Populate the `.env` file in the project root and fill it as shown [below](#env)
+- `npm install`
+
+Note: This app adds a couple npm modules to support icons and pwas
+- "@parcel/packager-raw-url"
+- "@parcel/transformer-webmanifest"
+
+Images are stored in src/dx-excshell-1/web-src/images/
---
+++ progressive-web-app/src/dx-excshell-1/actions/generic/index.js
@@ -0,0 +1,77 @@
+/*
+Copyright 2024 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+/**
+ * This is a sample action showcasing how to access an external API
+ *
+ * Note:
+ * You might want to disable authentication and authorization checks against Adobe Identity Management System for a generic action. In that case:
+ *   - Remove the require-adobe-auth annotation for this action in the manifest.yml of your application
+ *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
+ *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
+ *   - Make sure to validate these changes against your security requirements before deploying the action
+ */
+
+
+const fetch = require('node-fetch')
+const { Core } = require('@adobe/aio-sdk')
+const { errorResponse, getBearerToken, stringParameters, checkMissingRequestInputs } = require('../utils')
+
+// main function that will be executed by Adobe I/O Runtime
+async function main (params) {
+  // create a Logger
+  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+
+  try {
+    // 'info' is the default level if not set
+    logger.info('Calling the main action')
+
+    // log parameters, only if params.LOG_LEVEL === 'debug'
+    logger.debug(stringParameters(params))
+
+    // check for missing request input parameters and headers
+    const requiredParams = [/* add required params */]
+    const requiredHeaders = ['Authorization']
+    const errorMessage = checkMissingRequestInputs(params, requiredParams, requiredHeaders)
+    if (errorMessage) {
+      // return and log client errors
+      return errorResponse(400, errorMessage, logger)
+    }
+
+    // extract the user Bearer token from the Authorization header
+    const token = getBearerToken(params)
+
+    // replace this with the api you want to access
+    const apiEndpoint = 'https://adobeioruntime.net/api/v1'
+
+    // fetch content from external api endpoint
+    const res = await fetch(apiEndpoint)
+    if (!res.ok) {
+      throw new Error('request to ' + apiEndpoint + ' failed with status code ' + res.status)
+    }
+    const content = await res.json()
+    const response = {
+      statusCode: 200,
+      body: content
+    }
+
+    // log the response status code
+    logger.info(`${response.statusCode}: successful request`)
+    return response
+  } catch (error) {
+    // log any server errors
+    logger.error(error)
+    // return with 500
+    return errorResponse(500, 'server error', logger)
+  }
+}
+
+exports.main = main
